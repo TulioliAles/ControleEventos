bc310e0054642cbe43e698463ffb614fc01ea0c7
Alessandro Tulioli <desmecad@hotmail.com>
2020-06-03 17:30:31 -0300

    Commit 4 - Criada entidade Convidado e Form para inserir seus dados

---
+++ src/main/java/com/eventoapp/models/Convidado.java
@@ -0,0 +1,42 @@
+package com.eventoapp.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Convidado {
+	
+	@Id
+	private String rg;
+	private String nomeconvidado;
+	
+	@ManyToOne
+	private Evento evento;
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getNomeConvidado() {
+		return nomeconvidado;
+	}
+
+	public void setNomeConvidado(String nomeconvidado) {
+		this.nomeconvidado = nomeconvidado;
+	}
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+	
+	
+}
